7aad3c765824d19db4f1554db6106adaef97bba7
HTCINDIA\ramesh.pandi <ramesh.pandi@htcindia.com>
2022-11-06 01:14:13 +0530

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
     
-      Testing
+    <div className={styles.container}>
+    <Navbar></Navbar>
+      Testing 123 dsssds
+      <Footer></Footer>
     </div>
+    
   )
 }
