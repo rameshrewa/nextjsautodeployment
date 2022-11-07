3a6a57d1f663a3205438a96e59ca55e4411f555b
HTCINDIA\ramesh.pandi <ramesh.pandi@htcindia.com>
2022-11-07 10:22:01 +0530

    testing

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build'
+  distDir: 'build',
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 }
 
 module.exports = nextConfig
